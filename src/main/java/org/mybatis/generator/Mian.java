fb9d1079ab66167bffae56f8d32ebf4dcbf5a9b7
Amamiya_Asuka <1455085423@qq.com>
2018-02-12 17:57:20 +0800

    //保存

--- src/main/java/org/mybatis/generator/Mian.java
+++ src/main/java/org/mybatis/generator/Mian.java
@@ -25,28 +25,30 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 class Main {
 
     public static void main(String[] args) throws URISyntaxException {
-        try {
-            System.out.println("--------------------start generator-------------------");
-            List<String> warnings = new ArrayList<String>();
-            boolean overwrite = true;
-            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            InputStream is = classloader.getResourceAsStream("Config.xml");
-            ConfigurationParser cp = new ConfigurationParser(warnings);
-            Configuration config = cp.parseConfiguration(is);
-            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-            myBatisGenerator.generate(null);
-            System.out.println("--------------------end generator-------------------");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (InvalidConfigurationException e) {
-            e.printStackTrace();
-        } catch (XMLParserException e) {
-            e.printStackTrace();
-        }
+        System.out.println(Main.class.getResource("/"));
+
+//        try {
+//            System.out.println("--------------------start generator-------------------");
+//            List<String> warnings = new ArrayList<String>();
+//            boolean overwrite = true;
+//            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+//            InputStream is = classloader.getResourceAsStream("main/config.xml");
+//            ConfigurationParser cp = new ConfigurationParser(warnings);
+//            Configuration config = cp.parseConfiguration(is);
+//            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+//            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+//            myBatisGenerator.generate(null);
+//            System.out.println("--------------------end generator-------------------");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        } catch (InvalidConfigurationException e) {
+//            e.printStackTrace();
+//        } catch (XMLParserException e) {
+//            e.printStackTrace();
+//        }
     }
 }
---
+++ src/test/java/test.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+/**
+ * Created by Jamci on 2/10/2018.
+ */
+public class test{
+    public static void main(String[] args) {
+        System.out.println(test.class.getResource(""));
+    }
+}
